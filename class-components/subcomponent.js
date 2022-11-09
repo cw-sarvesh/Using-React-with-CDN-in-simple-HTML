f327bb14c89b56c93c7b526c8d7b44e412d9140d
Sarvesh Mishra <sarvesh.m@carwale.com>
2022-11-09 18:25:25 +0530

    how to reuse react component with react cdn

---
+++ class-components/subcomponent.js
@@ -0,0 +1,13 @@
+'use strict';
+
+class SubComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <span> SubComponent-is-working </span>;
+  }
+}
+
+customElements.define('subcomponent', SubComponent);
---
+++ function-components/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <Car />
+    </div>
+  );
+}
+ReactDOM.render(<MyApp />, document.getElementById('root-function'));
